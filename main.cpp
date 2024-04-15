2adc6c0c08ee61247fe0a1eae44ccb376f30f515
Kaden <kaden.larussa@selu.edu>
2024-04-14 22:17:21 -0500

    completed

--- main.cpp
+++ main.cpp
@@ -1,62 +1,52 @@
 #include <iostream>
-#include <vector>
 #include <fstream>
+#include <vector>
+#include <stack>
 
 class treeNode {
 public:
     int data;
     int count;
     treeNode *left, *right;
+
+    treeNode(int num) : data(num), count(1), left(nullptr), right(nullptr) {}
 };
 
-class tree{
+class Tree {
 public:
     treeNode *root;
-    tree() : root(nullptr) {}
+    int compareCount;
 
-    void inOrder(treeNode *t){
-        if (t != nullptr){
-            inOrder(t->left);
-            std::cout << t->data << " ";
-            inOrder(t->right);
-        }
+    Tree() : root(nullptr), compareCount(0) {}
+
+    treeNode* makeTreeNode(int num) {
+        return new treeNode(num);
     }
 
-
-    treeNode *makeTreeNode(int num){
-        treeNode *t = new treeNode();
-        t->data = num;
-        t->count = 1;
-        t->left = nullptr;
-        t->right = nullptr;
-        return t;
-    }
-
-    void buildTree(std::vector<int>& nums, treeNode*& root) {
-        for (int i = 0; i < nums.size(); i++) {
-            int num = nums[i];
-            if (i == 0) {
+    void buildTree(const std::vector<int>& nums) {
+        for (int num : nums) {
+            if (!root) {
                 root = makeTreeNode(num);
             } else {
-                bool searching = true;
-                treeNode* t = root;
-                while (searching) {
-                    if (num == t->data) {
-                        t->count++;
-                        searching = false;
-                    } else if (num < t->data) {
-                        if (t->left == nullptr) {
-                            t->left = makeTreeNode(num);
-                            searching = false;
+                treeNode* current = root;
+                while (true) {
+                    compareCount++;
+                    if (num == current->data) {
+                        current->count++;
+                        break;
+                    } else if (num < current->data) {
+                        if (!current->left) {
+                            current->left = makeTreeNode(num);
+                            break;
                         } else {
-                            t = t->left;
+                            current = current->left;
                         }
                     } else {
-                        if (t->right == nullptr) {
-                            t->right = makeTreeNode(num);
-                            searching = false;
+                        if (!current->right) {
+                            current->right = makeTreeNode(num);
+                            break;
                         } else {
-                            t = t->right;
+                            current = current->right;
                         }
                     }
                 }
@@ -64,21 +54,80 @@ public:
         }
     }
 
-    treeNode *root;
+    void inOrderRecursive(treeNode* node, std::ostream& os) {
+        if (!node) return;
+        inOrderRecursive(node->left, os);
+        os << node->data << " " << node->count << std::endl;
+        inOrderRecursive(node->right, os);
+    }
+
+    void inOrderIterative(std::ostream& os) {
+        std::stack<treeNode*> stack;
+        treeNode* current = root;
+        while (!stack.empty() || current) {
+            while (current) {
+                stack.push(current);
+                current = current->left;
+            }
+            current = stack.top();
+            stack.pop();
+            os << current->data << " " << current->count << std::endl;
+            current = current->right;
+        }
+    }
+
+    void writeTreeToFile(const std::string& filename) {
+        std::ofstream file(filename);
+        if (!file) {
+            std::cerr << "cant open" << std::endl;
+            return;
+        }
+        file << "Recursive inOrder:\n";
+        inOrderRecursive(root, file);
+
+        file << "Iterative inOrder :\n";
+        inOrderIterative(file);
+
+        file << "\nComparisons: " << compareCount << std::endl;
+        file.close();
+    }
 };
 
+void bubbleSort(std::vector<int>& nums, int& compareCount) {
+    bool swapped;
+    int n = nums.size();
+    for (int i = 0; i < n - 1; i++) {
+        swapped = false;
+        for (int j = 0; j < n - i - 1; j++) {
+            compareCount++;
+            if (nums[j] > nums[j + 1]) {
+                std::swap(nums[j], nums[j + 1]);
+                swapped = true;
+            }
+        }
+        if (!swapped) break;
+    }
+}
+
 int main() {
-
-    tree tree{};
-
     std::ifstream file("numbers.txt");
     std::vector<int> nums;
     int num;
-    while (file >> num){
+    while (file >> num) {
         nums.push_back(num);
     }
 
-    tree.buildTree(nums, tree.root);
+    Tree myTree;
+    myTree.buildTree(nums);
+    myTree.writeTreeToFile("sorted_tree_output.txt");
 
+    int compareCount = 0;
+    bubbleSort(nums, compareCount);
+    std::ofstream sortedFile("sorted_bubble_output.txt");
+    for (int number : nums) {
+        sortedFile << number << std::endl;
+    }
+    sortedFile << "\nTotal Comparisons: " << compareCount << std::endl;
 
+    return 0;
 }
